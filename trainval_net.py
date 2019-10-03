54d9e33df4d1923717dbbff91e2063f24a3f76ed
sunx <1059363093@qq.com>
2019-10-03 10:07:12 +0800

    ..

--- trainval_net.py
+++ trainval_net.py
@@ -33,7 +33,6 @@ from model.utils.net_utils import weights_normal_init, save_net, load_net, \
 from model.faster_rcnn.vgg16 import vgg16
 from model.faster_rcnn.resnet import resnet
 
-from test import test
 
 def parse_args():
   """
