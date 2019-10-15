83b1a7b5cc69c82157d83dba732a1e482af6bece
sunx <1059363093@qq.com>
2019-10-15 15:17:55 +0800

    late fuse

--- exp/model.py
+++ exp/model.py
@@ -59,10 +59,10 @@ class SpaLan(nn.Module):
             nn.Dropout(p=0.5),
             nn.Linear(1024, num_hoi_class))
 
-        self.spa_proposal = nn.Sequential(
-            nn.LeakyReLU(),
-            nn.Dropout(p=0.5),
-            nn.Linear(1024, 2))
+        # self.spa_proposal = nn.Sequential(
+        #     nn.LeakyReLU(),
+        #     nn.Dropout(p=0.5),
+        #     nn.Linear(1024, 2))
 
         self.obj_hidden_layer = nn.Sequential(
             nn.Linear(num_obj_class, 1024))
@@ -72,10 +72,10 @@ class SpaLan(nn.Module):
             nn.Dropout(p=0.5),
             nn.Linear(1024, num_hoi_class))
 
-        self.obj_proposal = nn.Sequential(
-            nn.LeakyReLU(),
-            nn.Dropout(p=0.5),
-            nn.Linear(1024, 2))
+        # self.obj_proposal = nn.Sequential(
+        #     nn.LeakyReLU(),
+        #     nn.Dropout(p=0.5),
+        #     nn.Linear(1024, 2))
 
         self.pose_hidden_layer = nn.Sequential(
             nn.Linear(num_key_point, 1024))
@@ -85,10 +85,10 @@ class SpaLan(nn.Module):
             nn.Dropout(p=0.5),
             nn.Linear(1024, num_hoi_class))
 
-        self.pose_proposal = nn.Sequential(
-            nn.LeakyReLU(),
-            nn.Dropout(p=0.5),
-            nn.Linear(1024, 2))
+        # self.pose_proposal = nn.Sequential(
+        #     nn.LeakyReLU(),
+        #     nn.Dropout(p=0.5),
+        #     nn.Linear(1024, 2))
 
         # self._initialize_weights()
 
@@ -97,38 +97,44 @@ class SpaLan(nn.Module):
 
         spa_vec = self.spa_conv(spa_map)
         spa_hidden = self.spa_hidden_layer(spa_vec)
-        spa_bin_scores = self.spa_proposal(spa_hidden)
         spa_hoi_scores = self.spa_classifier(spa_hidden)
+        spa_hoi_prob = F.sigmoid(spa_hoi_scores)
+        # spa_bin_scores = self.spa_proposal(spa_hidden)
 
         obj_hidden = self.obj_hidden_layer(obj_vec)
-        obj_bin_scores = self.obj_proposal(obj_hidden)
         obj_hoi_scores = self.spa_classifier(obj_hidden)
+        obj_hoi_prob = F.sigmoid(obj_hoi_scores)
+        # obj_bin_scores = self.obj_proposal(obj_hidden)
 
         pose_hidden = self.pose_hidden_layer(pose_feat)
-        pose_bin_scores = self.pose_proposal(pose_hidden)
         pose_hoi_scores = self.pose_classifier(pose_hidden)
+        pose_hoi_prob = F.sigmoid(pose_hoi_scores)
+        # pose_bin_scores = self.pose_proposal(pose_hidden)
 
-        bin_scores = (spa_bin_scores + obj_bin_scores + pose_bin_scores) / 3
-        hoi_scores = (spa_hoi_scores + obj_hoi_scores + pose_hoi_scores) / 3
+        # bin_scores = (spa_bin_scores + obj_bin_scores + pose_bin_scores) / 3
+        # hoi_scores = (spa_hoi_scores + obj_hoi_scores + pose_hoi_scores) / 3
 
-        bin_prob = F.softmax(bin_scores, dim=1)
-        hoi_prob = F.sigmoid(hoi_scores)
+        # bin_prob = F.softmax(bin_scores, dim=1)
+        bin_prob = torch.zeros(num_ins)
+        hoi_prob = spa_hoi_prob * obj_hoi_prob * pose_hoi_prob
 
-        bin_pred = torch.argmax(bin_prob, dim=1)
+        # bin_pred = torch.argmax(bin_prob, dim=1)
         hoi_pred = (hoi_prob > 0.5).float()
 
-        bin_error = torch.ones(1) * -1
-        hoi_error = torch.ones(1) * -1
+        bin_error = torch.zeros(num_ins)
+        hoi_error = torch.zeros(num_ins)
 
-        loss_cls = torch.ones(1) * -1
-        loss_bin = torch.ones(1) * -1
+        loss_cls = torch.zeros(num_ins)
+        loss_bin = torch.ones(num_ins)
 
         if hoi_cates is not None and bin_cates is not None:
-            bin_error = torch.abs(bin_pred - bin_cates).sum().float() / num_ins
+            # bin_error = torch.abs(bin_pred - bin_cates).sum().float() / num_ins
             hoi_error = torch.abs(hoi_pred[pos_mask] - hoi_cates[pos_mask]).sum() * 1.0 / pos_mask.sum().item()
-            loss_bin = F.cross_entropy(bin_scores, bin_cates, size_average=False)
-            loss_cls = F.binary_cross_entropy(hoi_prob[pos_mask], hoi_cates[pos_mask], size_average=False)    # multi-label classification
-
+            # loss_bin = F.cross_entropy(bin_scores, bin_cates, size_average=False)
+            spa_loss_cls = F.binary_cross_entropy(spa_hoi_prob[pos_mask], hoi_cates[pos_mask], size_average=False)    # multi-label classification
+            obj_loss_cls = F.binary_cross_entropy(obj_hoi_prob[pos_mask], hoi_cates[pos_mask], size_average=False)
+            pos_loss_cls = F.binary_cross_entropy(pose_hoi_prob[pos_mask], hoi_cates[pos_mask], size_average=False)
+            loss_cls = spa_loss_cls + obj_loss_cls + pos_loss_cls
         return bin_prob, hoi_prob, loss_bin, loss_cls, bin_error, hoi_error
 
 
