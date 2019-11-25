71fb6b16f958fa0b8132d92e6e1c9404395d760a
sunx <1059363093@qq.com>
2019-11-25 21:17:29 +0800

    fix "out of memory"

--- lib/roi_data_layer/roibatchLoader.py
+++ lib/roi_data_layer/roibatchLoader.py
@@ -235,14 +235,16 @@ class roibatchLoader(data.Dataset):
 
         assert keep3.shape[0] == keep.shape[0] * 3
 
-        gt_boxes = gt_boxes[keep3]
-        gt_classes = gt_classes[keep]
-        gt_verbs = gt_verbs[keep]
-        gt_binaries = gt_binaries[keep]
-        gt_spa_maps = gt_spa_maps[keep]
-        gt_hoi_masks = gt_hoi_masks[keep]
-        gt_vrb_masks = gt_vrb_masks[keep]
-        gt_pose_maps = gt_pose_maps[keep]
+        if not_keep.sum() > 0:
+
+            gt_boxes = gt_boxes[keep3]
+            gt_classes = gt_classes[keep]
+            gt_verbs = gt_verbs[keep]
+            gt_binaries = gt_binaries[keep]
+            gt_spa_maps = gt_spa_maps[keep]
+            gt_hoi_masks = gt_hoi_masks[keep]
+            gt_vrb_masks = gt_vrb_masks[keep]
+            gt_pose_maps = gt_pose_maps[keep]
 
         gt_num_boxes = int(gt_boxes.size(0) / 3)
         assert gt_num_boxes * 3 == gt_boxes.size(0)
