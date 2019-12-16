b4d2f385c16018711ad94dc06424bde92913337e
sunx <1059363093@qq.com>
2019-12-16 10:03:25 +0800

    frequency

--- analyze/analyze_results.py
+++ analyze/analyze_results.py
@@ -1,4 +1,5 @@
 import os
+import pickle
 import scipy.io as sio
 import numpy as np
 import matplotlib.pyplot as plt
@@ -13,18 +14,21 @@ hoi_labels = np.array([hoi.hoi_name() for hoi in hoi_classes])
 exp_name = 'rcnn_w2v_init_8ep'
 eval_def_path = os.path.join(PROJECT_ROOT, 'output', 'results', exp_name, 'eval_result_def.mat')
 eval_ko_path = os.path.join(PROJECT_ROOT, 'output', 'results', exp_name, 'eval_result_ko.mat')
+hoi_freq_path = os.path.join(DS_ROOT, 'hoi_frequencies.pkl')
 
+with open(hoi_freq_path) as f:
+    hoi_freqs = pickle.load(f)
 
 eval_def = sio.loadmat(eval_def_path)
 def_recs = eval_def['REC'].reshape(600)
 def_aps = eval_def['AP'].reshape(600)
 
-def_order = np.argsort(def_recs)[::-1]
+def_order = np.argsort(hoi_freqs)[::-1]
+hoi_freqs = hoi_freqs[def_order]
 def_hois = hoi_labels[def_order]
 def_aps = def_aps[def_order]
 def_recs = def_recs[def_order]
 
-color_list = ['b', 'g', 'r']
 
 for i in range(0, 12):
     fig = plt.figure(figsize=(15, 10))
@@ -33,22 +37,16 @@ for i in range(0, 12):
     hois = def_hois[i*50: (i+1)*50]
     recs = def_recs[i*50: (i+1)*50]
     aps = def_aps[i*50: (i+1)*50]
+    freqs = hoi_freqs[i*50: (i+1)*50]
 
     ax1 = fig.subplots()
     xlocation = np.linspace(1, len(hois) * 0.6, len(hois))
-    rects01 = ax1.bar(xlocation, aps, width=0.2, color='b', linewidth=1, alpha=0.8)
-    rects02 = ax1.bar(xlocation + 0.2, recs, width=0.2, color='g', linewidth=1, alpha=0.8)
+    rects03 = ax1.bar(xlocation + 0.0, freqs, width=0.2, color='r', linewidth=1, alpha=0.8)
+    rects01 = ax1.bar(xlocation + 0.2, aps, width=0.2, color='b', linewidth=1, alpha=0.8)
+    rects02 = ax1.bar(xlocation + 0.4, recs, width=0.2, color='g', linewidth=1, alpha=0.8)
     plt.xticks(xlocation + 0.15, hois, fontsize=12, rotation=90)
     plt.show()
 
-
-
-
-
-
-eval_ko = sio.loadmat(eval_ko_path)
-eval_ko_rec = eval_ko['REC']
-eval_ko_ap = eval_ko['AP']
 
 
 
