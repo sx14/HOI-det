655412ac7851edcf8ee39f3d2d13dfa41f476bc9
sunx <1059363093@qq.com>
2019-12-25 22:43:39 +0800

    ..

--- lib/model/faster_rcnn/SFT_layer.py
+++ lib/model/faster_rcnn/SFT_layer.py
@@ -13,17 +13,17 @@ import torch.nn.functional as F
 class SFTLayer(nn.Module):
     def __init__(self):
         super(SFTLayer, self).__init__()
-        self.SFT_scale_conv0 = nn.Linear(300, 1024)
-        self.SFT_scale_conv1 = nn.Linear(1024, 2048)
-        self.SFT_shift_conv0 = nn.Linear(300, 1024)
-        self.SFT_shift_conv1 = nn.Linear(1024, 2048)
+        self.SFT_scale_conv0 = nn.Linear(300, 512)
+        self.SFT_scale_conv1 = nn.Linear(512, 1024)
+        self.SFT_shift_conv0 = nn.Linear(300, 512)
+        self.SFT_shift_conv1 = nn.Linear(512, 1024)
 
     def forward(self, feat, objvec):
         # x[0]: fea; x[1]: cond
         scale = self.SFT_scale_conv1(F.leaky_relu(self.SFT_scale_conv0(objvec), 0.1, inplace=True))
-        scale = scale.view((-1, 2048, 1, 1))
+        scale = scale.view((-1, 1024, 1, 1))
         shift = self.SFT_shift_conv1(F.leaky_relu(self.SFT_shift_conv0(objvec), 0.1, inplace=True))
-        shift = shift.view((-1, 2048, 1, 1))
+        shift = shift.view((-1, 1024, 1, 1))
         return (feat * scale + shift) + feat
 
 
