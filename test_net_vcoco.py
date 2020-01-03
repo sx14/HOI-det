38719e2b55049457935fc1046d26e21f049f83a9
sunx <1059363093@qq.com>
2020-01-04 02:32:30 +0800

    Base0

--- test_net_vcoco.py
+++ test_net_vcoco.py
@@ -162,7 +162,7 @@ if __name__ == '__main__':
   if not os.path.exists(input_dir):
     raise Exception('There is no input directory for loading network from ' + input_dir)
   load_name = os.path.join(input_dir,
-    'base0_cb_{}_{}_{}.pth'.format(args.checksession, args.checkepoch, args.checkpoint))
+    'base0_{}_{}_{}.pth'.format(args.checksession, args.checkepoch, args.checkpoint))
 
   obj_classes, vrb_classes, obj2ind, vrb2ind = vcoco.load_hoi_classes(cfg.DATA_DIR + '/vcoco')
   obj_class_map = vcoco.load_object_class_map(cfg.DATA_DIR + '/vcoco')
